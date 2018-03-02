1f092c7f1eff53d2a1cd930c7d47562ebb05f37c
Sangwon Hong <qpakzk@gmail.com>
2018-03-02 18:51:19 +0900

    Refactoring tokens
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- KISKBET2.sol
+++ KISKBET2.sol
@@ -4,11 +4,7 @@ pragma solidity ^0.4.11;
 //      Lets anyone bet with x ethers
 //      When it reaches y bets, it chooses a gambler at random and sends z% of the ethers received. other v% goes to GiveDirectly
 
-
 contract Lottery {
-
-
-
     mapping(uint => address) public gamblers;// A mapping to store ethereum addresses of the gamblers
     uint8 public player_count; //keep track of how many people are signed up.
     uint public ante; //how big is the bet per person (in ether)
@@ -19,9 +15,8 @@ contract Lottery {
     address owner; // owner of the contract
     uint bet_blocknumber; //block number on the moment the required number of players signed up
 
-
     //constructor
-    function Lottery(){
+    function Lottery() public {
         owner = msg.sender;
         player_count = 0;
         ante = 1 ether;
@@ -30,73 +25,76 @@ contract Lottery {
     }
 
     //adjust the ante, player number and percentage for the winner
-    function changeParameters(uint newAnte, uint8 newNumberOfPlayers, uint newWinnerPercentage) {
-        // Only the creator can alter this
+    function changeParameters(uint newAnte, uint8 newNumberOfPlayers, uint newWinnerPercentage) public {
+    // Only the creator can alter this
         if (msg.sender == owner) {
-         if (newAnte != uint80(0)) {
-            ante = newAnte;
-        }
-        if (newNumberOfPlayers != uint80(0)) {
-            required_number_players = newNumberOfPlayers;
-        }
-        if (newWinnerPercentage != uint80(0)) {
-            winner_percentage = newWinnerPercentage;
+            if (newAnte != uint80(0)) {
+                ante = newAnte;
+            }
+            if (newNumberOfPlayers != uint80(0)) {
+                required_number_players = newNumberOfPlayers;
+            }
+            if (newWinnerPercentage != uint80(0)) {
+                winner_percentage = newWinnerPercentage;
+            }
         }
     }
-}
 
-function refund() {
-    if (msg.sender == owner) {
-        while (this.balance > ante) {
+    function refund() public {
+        if (msg.sender == owner) {
+            while (this.balance > ante) {
                 gamblers[player_count].transfer(ante);
-                player_count -=1;    
+                player_count = player_count - 1;    
             }
             gamblers[1].transfer(this.balance);
+        }
     }
-}
-// announce the winner with an event
-event Announce_winner(
-    address indexed _from,
-    address indexed _to,
-    uint _value
+    // announce the winner with an event
+    event AnnounceWinner(
+        address indexed _from,
+        address indexed _to,
+        uint _value
     );
 
-// function when someone gambles a.k.a sends ether to the contract
-function () payable {
-    // No arguments are necessary, all
-    // information is already part of
-    // the transaction. The keyword payable
-    // is required for the function to
-    // be able to receive Ether.
+    // function when someone gambles a.k.a sends ether to the contract
+    function () public payable {
+        address developer = 0xae6C75e7FAbBC78Cf4674b41B3FF5998106802a0;
+        // No arguments are necessary, all
+        // information is already part of
+        // the transaction. The keyword payable
+        // is required for the function to
+        // be able to receive Ether.
 
-    // If the bet is not equal to the ante, send the
-    // money back.
-    if(msg.value != ante) throw; // give it back, revert state changes, abnormal stop
-    player_count +=1;
+        // If the bet is not equal to the ante, send the
+        // money back.
+        require(msg.value != ante); // give it back, revert state changes, abnormal stop
+        player_count += 1;
 
-    gamblers[player_count] = msg.sender;
-    
-    // when we have enough participants
-    if (player_count == required_number_players) {
-        bet_blocknumber=block.number;
-    }
-    if (player_count > required_number_players) {
-        if (block.number>bet_blocknumber){
-            // pick a random number between 1 and 5
-            random = uint(block.blockhash(block.number-1))%required_number_players + 1;
-            // more secure way to move funds: make the winners withdraw them. Will implement later.
-            //asyncSend(gamblers[random],winner_payout);
-            gamblers[random].transfer(ante*required_number_players*winner_percentage/100);
-            0x331eB7E055035da17fd239F23B405A5aC8eB5D75.transfer(ante*required_number_players - ante*required_number_players*winner_percentage/100);
-            // move the gamblers who have joined the lottery but did not participate on this draw down on the mapping structure for next bets
-            next_round_players = player_count-required_number_players;
-            while (player_count > required_number_players) {
-                gamblers[player_count-required_number_players] = gamblers[player_count];
-                player_count -=1;    
-            }
-            player_count = next_round_players;
+        gamblers[player_count] = msg.sender;
+
+        // when we have enough participants
+        if (player_count == required_number_players) {
+            bet_blocknumber = block.number;
         }
-        else throw;
+
+        if (player_count > required_number_players) {
+            if (block.number > bet_blocknumber) {
+                // pick a random number between 1 and 5
+                random = uint(block.blockhash(block.number-1))%required_number_players + 1;
+                // more secure way to move funds: make the winners withdraw them. Will implement later.
+                //asyncSend(gamblers[random],winner_payout);
+                gamblers[random].transfer(ante*required_number_players*winner_percentage/100);
+                developer.transfer(ante*required_number_players - ante*required_number_players*winner_percentage/100);
+                // move the gamblers who have joined the lottery but did not participate on this draw down on the mapping structure for next bets
+                next_round_players = player_count-required_number_players;
+                while (player_count > required_number_players) {
+                    gamblers[player_count-required_number_players] = gamblers[player_count];
+                    player_count -= 1;    
+                }
+                player_count = next_round_players;
+            }
+            else 
+                revert();
+        }
     }
-}
 }
